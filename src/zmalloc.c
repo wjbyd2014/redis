b559347f569dad5b67abac951747bbafbe23afd8
nefu_wjb <176930292@qq.com>
2015-06-17 15:29:58 +0800

    test git

--- src/zmalloc.c
+++ src/zmalloc.c
@@ -113,7 +113,7 @@ static int zmalloc_thread_safe = 0;
 pthread_mutex_t used_memory_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 static void zmalloc_default_oom(size_t size) {
-    fprintf(stderr, "zmalloc: Out of memory trying to allocate %zu bytes\n",
+    fprintf(stderr, "zmalloc~~~: Out of memory trying to allocate %zu bytes\n",
         size);
     fflush(stderr);
     abort();
